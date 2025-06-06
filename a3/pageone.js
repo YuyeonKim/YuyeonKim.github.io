3720e149a2ce3f48d246f105c97485fe1491e504
YuyeonKim <s4101278@student.rmit.edu.au>
2025-06-06 12:54:47 +1000

    commit

---
+++ a3/pageone.js
@@ -0,0 +1,5 @@
+const memoryGameButton = document.querySelector("#memory-game-button");
+console.log(memoryGameButton);
+memoryGameButton.addEventListener("click", function () {
+  location.href = "index.html";
+});
